8aa8e31cf098ee757d093047fc7025dcd768ad62
Maksymilian Buk <maksymilian.buk@wp.pl>
2019-08-14 10:01:55 +0200

    Add necessary comments. Reformat code

--- app/src/main/java/com/buk/tictactoe/MainActivity.java
+++ app/src/main/java/com/buk/tictactoe/MainActivity.java
@@ -2,119 +2,125 @@ package com.buk.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import androidx.gridlayout.widget.GridLayout;
+//import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+   //Fields are name like:
+   //0 1 2
+   //3 4 5
+   //6 7 8
+   static Color[] scoreArray = new Color[9];
+   static boolean redTurn = true;
+   static int clickCount;
+   LinearLayout linearLayout;
+   GridLayout gridLayout;
+   TextView resultNotification;
 
-    //Fields are name like:
-    //0 1 2
-    //3 4 5
-    //6 7 8
-    static Color[] scoreArray=new Color[9];
-    static boolean redDrop=true;
-    static int clickCount;
-    LinearLayout linearLayout;
-    TextView resultNotification;
+   //Handle player's choice
+   public void dropIn(View view)
+   {
+      ImageView clicked = (ImageView) view;
+      //Check whose turn right now and set proper image.
+      if (redTurn)
+      {
+         clicked.setImageResource(R.drawable.red);
+      } else
+      {
+         clicked.setImageResource(R.drawable.green);
+      }
 
+      //Animation block of dropping down item. If clicked on the highest line- item will climb up from bottom.
+      if (clicked.getTag().toString().equals("0") || clicked.getTag().toString().equals("1") || clicked.getTag().toString().equals("2"))
+      {
+         clicked.setTranslationY(1000f);
+         clicked.animate().translationYBy(-1000f).rotationBy(360f).setDuration(1000);
+         clicked.setClickable(false);
+      } else
+      {
+         clicked.setTranslationY(-1000f);
+         clicked.animate().translationYBy(1000f).rotationBy(360f).setDuration(1000);
+         clicked.setClickable(false);
+      }
 
-    public void dropIn(View view)
-    {
+      //Increment total number of turns.
+      clickCount++;
 
-        ImageView clicked= (ImageView) view;
-        if(redDrop)
-        {
-            clicked.setImageResource(R.drawable.red);
-        }
-        else
-        {
-            clicked.setImageResource(R.drawable.green);
-        }
+      //Check if someone wins by validating return value. Show proper notification with animation.
+      //Green wins.
+      if (MainService.checkGame(clicked.getTag().toString(), scoreArray, redTurn) == Color.GREEN)
+      {
+         linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+         resultNotification = (TextView) findViewById(R.id.resultNotafication);
+         resultNotification.setText("Green player wins");
+         linearLayout.setBackgroundColor(android.graphics.Color.parseColor("#0d6134"));
+         //linearLayout.setBackgroundColor(0x0d6134);
+         linearLayout.animate().alpha(1f).setDuration(1000);
+         linearLayout.setVisibility(View.VISIBLE);
+         MainService.disableGrid(gridLayout);
+      }
+      //Red wins.
+      if (MainService.checkGame(clicked.getTag().toString(), scoreArray, redTurn) == Color.RED)
+      {
+         linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+         resultNotification = (TextView) findViewById(R.id.resultNotafication);
+         resultNotification.setText("Red player wins");
+         linearLayout.setBackgroundColor(android.graphics.Color.parseColor("#730707"));
+         //linearLayout.setBackgroundColor(0x730707);
+         linearLayout.animate().alpha(1f).setDuration(1000);
+         linearLayout.setVisibility(View.VISIBLE);
+         MainService.disableGrid(gridLayout);
+      }
+      //Check if it is a draw by validating total number of turns and show notification.
+      if (clickCount >= 9)
+      {
+         linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+         resultNotification = (TextView) findViewById(R.id.resultNotafication);
+         resultNotification.setText("It's a draw! Try again!");
+         resultNotification.setTextColor(android.graphics.Color.parseColor("#000000"));
+         //resultNotification.setTextColor(0x00000);
+         linearLayout.setBackgroundColor(android.graphics.Color.parseColor("#7b8087"));
+         //linearLayout.setBackgroundColor(0x7b8087);
+         linearLayout.animate().alpha(1f).setDuration(1000);
+         linearLayout.setVisibility(View.VISIBLE);
+         //MainService.disableGrid(gridLayout);
+         //^All buttons are disable anyway
+      }
+      //Change player1 into player2 (Red into Green or vice versa)
+      redTurn = !redTurn;
+   }
 
-        if(clicked.getTag().toString().equals("0")||clicked.getTag().toString().equals("1")||clicked.getTag().toString().equals("2"))
-        {
-            clicked.setTranslationY(1000f);
-            clicked.animate().translationYBy(-1000f).rotationBy(360f).setDuration(1000);
-            clicked.setClickable(false);
-        }
-        else
-        {
-            clicked.setTranslationY(-1000f);
-            clicked.animate().translationYBy(1000f).rotationBy(360f).setDuration(1000);
-            clicked.setClickable(false);
-        }
+   //Start new game.
+   public void playAgain(View view)
+   {
+      for (int i = 0; i < 9; i++)
+      {
+         scoreArray[i] = Color.NONE;
+      }
+      clickCount = 0;
+      findViewById(R.id.linearLayout).setVisibility(View.INVISIBLE);
+      linearLayout.setAlpha(0f);
+      gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+      MainService.enableGrid(gridLayout);
+      MainService.resetImages(gridLayout);
+   }
 
-
-        clickCount++;
-        if (MainService.checkGame(clicked.getTag().toString(), scoreArray, redDrop)==Color.GREEN)
-        {
-            linearLayout=(LinearLayout) findViewById(R.id.linearLayout);
-            resultNotification =(TextView) findViewById(R.id.resultNotafication);
-            resultNotification.setText("Green player wins");
-            linearLayout.setBackgroundColor(android.graphics.Color.parseColor("#0d6134"));
-            //linearLayout.setBackgroundColor(0x0d6134);
-            linearLayout.animate().alpha(1f).setDuration(1000);
-            linearLayout.setVisibility(View.VISIBLE);
-        }
-        if (MainService.checkGame(clicked.getTag().toString(), scoreArray, redDrop)==Color.RED)
-        {
-            linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
-            resultNotification = (TextView) findViewById(R.id.resultNotafication);
-            resultNotification.setText("Red player wins");
-            linearLayout.setBackgroundColor(android.graphics.Color.parseColor("#730707"));
-            //linearLayout.setBackgroundColor(0x730707);
-            linearLayout.animate().alpha(1f).setDuration(1000);
-            linearLayout.setVisibility(View.VISIBLE);
-        }
-        if(clickCount>=9)
-        {
-            linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
-            resultNotification = (TextView) findViewById(R.id.resultNotafication);
-            resultNotification.setText("It's a draw! Try again!");
-            resultNotification.setTextColor(android.graphics.Color.parseColor("#000000"));
-            //resultNotification.setTextColor(0x00000);
-            linearLayout.setBackgroundColor(android.graphics.Color.parseColor("#7b8087"));
-            //linearLayout.setBackgroundColor(0x7b8087);
-            linearLayout.animate().alpha(1f).setDuration(1000);
-            linearLayout.setVisibility(View.VISIBLE);
-        }
-
-        //MainService.showToasterAndStopGame(getApplicationContext(),view,MainService.checkGame(clicked.getTag().toString(),scoreArray,redDrop));
-
-        redDrop=!redDrop;
-    }
-
-    public void playAgain(View view)
-    {
-        for(int i=0;i<9;i++)
-        {
-            scoreArray[i]=Color.NONE;
-        }
-        setContentView(R.layout.activity_main);
-        clickCount=0;
-        findViewById(R.id.linearLayout).setVisibility(View.INVISIBLE);
-        linearLayout.setAlpha(0f);
-    }
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        clickCount=0;
-        findViewById(R.id.linearLayout).setVisibility(View.INVISIBLE);
-//        for(Color value:scoreArray)
-//        {
-//            value=Color.NONE;
-//        }
-        for(int i=0;i<9;i++)
-        {
-            scoreArray[i]=Color.NONE;
-        }
-    }
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      clickCount = 0;
+      findViewById(R.id.linearLayout).setVisibility(View.INVISIBLE);
+      for (int i = 0; i < 9; i++)
+      {
+         scoreArray[i] = Color.NONE;
+      }
+   }
 }
--- app/src/main/java/com/buk/tictactoe/MainService.java
+++ app/src/main/java/com/buk/tictactoe/MainService.java
@@ -1,76 +1,82 @@
 package com.buk.tictactoe;
+//import android.widget.GridLayout;
 
-import android.content.Context;
+import androidx.gridlayout.widget.GridLayout;
+
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public abstract class MainService {
-    static Color checkGame(String positionTag,Color [] scoreArray, boolean red)
-    {
-        //Set position to scoreArray
-        int position=Integer.parseInt(positionTag);
-        if(red)
-        {
-            scoreArray[position]=Color.RED;
-        }
-        else
-        {
-            scoreArray[position]=Color.GREEN;
-        }
+public abstract class MainService
+{
+   //Check game status. Return Color.NONE if no one wins. Otherwise- return color of winner (e.g. Color.RED if Red player wins)
+   static Color checkGame(String positionTag, Color[] scoreArray, boolean red)
+   {
+      //Set position to scoreArray
+      int position = Integer.parseInt(positionTag);
+      if (red)
+      {
+         scoreArray[position] = Color.RED;
+      } else
+      {
+         scoreArray[position] = Color.GREEN;
+      }
 
-        //Check if someone wins
-        //Just remind:
-        //Fields are name like:
-        //0 1 2
-        //3 4 5
-        //6 7 8
-        if
-        ((scoreArray[0] == scoreArray[1]) && (scoreArray[1] == scoreArray[2]) && (scoreArray[0]!=Color.NONE)
-                || (scoreArray[3] == scoreArray[4]) && (scoreArray[4] == scoreArray[5]) && (scoreArray[3]!=Color.NONE)
-                || (scoreArray[6] == scoreArray[7]) && (scoreArray[7] == scoreArray[8]) && (scoreArray[6]!=Color.NONE)
-                || (scoreArray[0] == scoreArray[3]) && (scoreArray[3] == scoreArray[6]) && (scoreArray[0]!=Color.NONE)
-                || (scoreArray[1] == scoreArray[4]) && (scoreArray[4] == scoreArray[7]) && (scoreArray[1]!=Color.NONE)
-                || (scoreArray[2] == scoreArray[5]) && (scoreArray[5] == scoreArray[8]) && (scoreArray[2]!=Color.NONE)
-                || (scoreArray[0] == scoreArray[4]) && (scoreArray[4] == scoreArray[8]) && (scoreArray[0]!=Color.NONE)
-                || (scoreArray[2] == scoreArray[4]) && (scoreArray[4] == scoreArray[6]) && (scoreArray[2]!=Color.NONE)
-        )  {
-            if (red) {
-                return Color.RED;
-            } else {
-                return Color.GREEN;
-            }
-        }
-        return Color.NONE;
-    }
+      //Check if someone wins
+      //Just remind:
+      //Fields are name like:
+      //0 1 2
+      //3 4 5
+      //6 7 8
 
-    static void showToasterAndStopGame(Context context,View view, Color winPlayer)
-    {
-        if(winPlayer==Color.RED)
-        {
-            Toast toast= Toast.makeText(context, "RED PLAYER WINS!\nFLAWLESS VICTORY!", Toast.LENGTH_LONG);
+      //Check if any line has got the same color except Color.NONE.
+      if
+      ((scoreArray[0] == scoreArray[1]) && (scoreArray[1] == scoreArray[2]) && (scoreArray[0] != Color.NONE)
+              || (scoreArray[3] == scoreArray[4]) && (scoreArray[4] == scoreArray[5]) && (scoreArray[3] != Color.NONE)
+              || (scoreArray[6] == scoreArray[7]) && (scoreArray[7] == scoreArray[8]) && (scoreArray[6] != Color.NONE)
+              || (scoreArray[0] == scoreArray[3]) && (scoreArray[3] == scoreArray[6]) && (scoreArray[0] != Color.NONE)
+              || (scoreArray[1] == scoreArray[4]) && (scoreArray[4] == scoreArray[7]) && (scoreArray[1] != Color.NONE)
+              || (scoreArray[2] == scoreArray[5]) && (scoreArray[5] == scoreArray[8]) && (scoreArray[2] != Color.NONE)
+              || (scoreArray[0] == scoreArray[4]) && (scoreArray[4] == scoreArray[8]) && (scoreArray[0] != Color.NONE)
+              || (scoreArray[2] == scoreArray[4]) && (scoreArray[4] == scoreArray[6]) && (scoreArray[2] != Color.NONE))
+      {
+         if (red)
+         {
+            return Color.RED;
+         } else
+         {
+            return Color.GREEN;
+         }
+      }
+      return Color.NONE;
+   }
 
-//            view=toast.getView();
-//            view.setBackgroundColor(android.graphics.Color.parseColor("9c0505"));
-//            TextView text = (TextView) view.findViewById(android.R.id.message);
-//            text.setTextColor(android.graphics.Color.parseColor("#ebe6e6"));
-//            text.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            toast.show();
-        }
-        if(winPlayer==Color.GREEN)
-        {
-            Toast toast= Toast.makeText(context, "GREEN PLAYER WINS!\nFLAWLESS VICTORY!", Toast.LENGTH_SHORT);
-//            View view=toast.getView();
-//           view.setBackgroundColor(android.graphics.Color.parseColor("9c0505"));
-//            TextView text = (TextView) view.findViewById(android.R.id.message);
-//            text.setTextColor(android.graphics.Color.parseColor("#ebe6e6"));
-//            //text.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            toast.show();
-        }
 
-        //Need to find how to stop game and block rest of clicks... Disable all buttons? Thread stop?
+   //Disable all ImageViews in GridLayout
+   static void disableGrid(GridLayout gridLayout)
+   {
+      for (int i = 0; i < gridLayout.getChildCount(); i++)
+      {
+         ((ImageView) gridLayout.getChildAt(i)).setClickable(false);
+      }
+   }
 
-    }
+   //Enable all ImageViews in GridLayout
+   static void enableGrid(GridLayout gridLayout)
+   {
+      for (int i = 0; i < gridLayout.getChildCount(); i++)
+      {
+         ((ImageView) gridLayout.getChildAt(i)).setClickable(true);
+      }
+   }
+
+   //Reset game board to blank by deleting resources of all images.
+   static void resetImages(GridLayout gridLayout)
+   {
+      for (int i = 0; i < gridLayout.getChildCount(); i++)
+      {
+         ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+      }
+   }
 }
